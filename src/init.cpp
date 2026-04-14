d7b8754dd0f6204342296cc9a6b8d7c7292c3049
Victor Groux <victorgroux99@gmail.com>
2026-04-14 09:47:37 +0200

    Init OK

--- src/init.cpp
+++ src/init.cpp
@@ -1,21 +1,60 @@
 #include "glad/glad.h"
+#include "opengl_error.h"
+
 #include "init.h"
 
 #include <stdexcept>
+#include <iostream>
 
-static void	initGLAD(GLFWwindow *win)
+static void	error_cb(int error, const char* description)
 {
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	std::cerr << "Error " << error << ": " << description << std::endl;
+}
+
+static void	debug_cb(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *message, const void *userParam)
+{
+	(void)source;
+	(void)type;
+	(void)id;
+	(void)length;
+	(void)userParam;
+
+	if (severity == GL_DEBUG_SEVERITY_NOTIFICATION)
+		return;
+
+	const char* severityStr = "UNKNOWN";
+	switch (severity)
 	{
-		clearOpenGLInstance(win);
-		throw std::runtime_error("[GLAD] Failed to initialize");
+		case GL_DEBUG_SEVERITY_HIGH:
+			severityStr = "HIGH";
+			break;
+		case GL_DEBUG_SEVERITY_MEDIUM:
+			severityStr = "MEDIUM";
+			break;
+		case GL_DEBUG_SEVERITY_LOW:
+			severityStr = "LOW";
+			break;
+		default:
+			break;
 	}
+	std::cerr << "OpenGL [" << severityStr << "]: " << message << std::endl;
+}
+
+static void	key_cb(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	(void)scancode;
+	(void)mods;
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
 GLFWwindow*	createWindow(void)
 {
 	if (!glfwInit())
 		throw std::runtime_error("[GLFW] Failed to initialize");
+
+	glfwSetErrorCallback(error_cb);
 
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -33,9 +72,17 @@ GLFWwindow*	createWindow(void)
 		throw std::runtime_error("[GLFW] Failed to create window");
 	}
 
+	glfwSetKeyCallback(win, key_cb);
 	glfwMakeContextCurrent(win);
 
-	initGLAD(win);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		clearOpenGLInstance(win);
+		throw std::runtime_error("[GLAD] Failed to initialize");
+	}
+
+	cgl(glEnable(GL_DEPTH_TEST));
+	cgl(glDebugMessageCallback(debug_cb, nullptr));
 
 	return win;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
-#include "init.hpp"
+#include "opengl_error.h"
+
+
+#include "init.h"
 
 #include <iostream>
 
@@ -7,9 +10,27 @@ int	main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 
-	std::cout << "hello world" << std::endl;
+	GLFWwindow*	window;
+	try
+	{
+		window = createWindow();
+	}
+	catch (std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
 
-	GLFWwindow*	window = createWindow();
+	int	width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	cgl(glViewport(0, 0, width, height));
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
 
+		glfwSwapBuffers(window);
+	}
+
+	clearOpenGLInstance(window);
 	return 0;
 }
