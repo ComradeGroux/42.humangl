fc654341b62d4a5a3350810a50e037d1beb93eed
Victor Groux <victorgroux99@gmail.com>
2026-05-18 07:58:04 +0200

    Angle converting functions now inline

--- matrix/include/utils.hpp
+++ matrix/include/utils.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
 namespace matrix {
-	inline float	deg_to_radians(float degrees);
-	inline float	rad_to_degrees(float radians);
+	inline float	deg_to_radians(float degrees) {
+		return degrees * 3.14159265358979323846f / 180.0f;
+	};
+	inline float	rad_to_degrees(float radians) {
+		return radians * 180.0f / 3.14159265358979323846f;
+	};
+
 	float			clamp(float value, float min, float max);
 	float			smoothstep(float edge0, float edge1, float t);
 }
--- matrix/src/utils.cpp
+++ matrix/src/utils.cpp
@@ -1,15 +1,5 @@
 #include "matrix.hpp"
 
-float matrix::deg_to_radians(float degrees)
-{
-	return degrees * 3.14159265358979323846f / 180.0f;
-}
-
-float matrix::rad_to_degrees(float radians)
-{
-	return radians * 180.0f / 3.14159265358979323846f;
-}
-
 float	matrix::clamp(float value, float min, float max)
 {
 	if (value < min)
