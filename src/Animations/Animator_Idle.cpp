b0a8798c8de3c9ed3b09c95f652c8b7522b97063
Victor Groux <victorgroux99@gmail.com>
2026-05-22 16:09:59 +0200

    Idle animation is ok

--- src/Animations/Animator_Idle.cpp
+++ src/Animations/Animator_Idle.cpp
@@ -110,32 +110,32 @@ std::list<Animator::KeyFrame>	Animator::_createIdle(void)
 				{
 					BoneNode::body_part::UPPER_ARM_LEFT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = { 0.0f, 0.0f, -0.05f },
+						.rotation = { 1.0f, 0.05f, 0.0f, 0.0f },
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::LOWER_ARM_LEFT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = {},
+						.rotation = {},
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::UPPER_ARM_RIGHT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = { 0.0f, 0.0f, 0.05f },
+						.rotation = { 1.0f, -0.05f, 0.0f, 0.0f },
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::LOWER_ARM_RIGHT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = {},
+						.rotation = {},
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
