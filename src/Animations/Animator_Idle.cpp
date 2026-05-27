fbf0f724c3513eea99d05f5133ba738eda0cc00d
Victor Groux <victorgroux99@gmail.com>
2026-05-27 11:28:24 +0200

    Idle animations is now OK

--- src/Animations/Animator_Idle.cpp
+++ src/Animations/Animator_Idle.cpp
@@ -17,7 +17,92 @@ std::list<Animator::KeyFrame>	Animator::_createIdle(void)
 				{
 					BoneNode::body_part::CHEST,
 					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_ARM_LEFT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_ARM_LEFT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_ARM_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_ARM_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_LEG_LEFT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_LEG_LEFT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_LEG_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_LEG_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				}
+			}
+		},
+		{
+			.duration = 0.5f,
+			.movement = {
+				{
+					BoneNode::body_part::HEAD,
+					{
 						.pos = { 0.0f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, -0.025f, 0.0f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::CHEST,
+					{
+						.pos = { 0.0f, -0.1f, 0.0f },
 						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
@@ -25,64 +110,64 @@ std::list<Animator::KeyFrame>	Animator::_createIdle(void)
 				{
 					BoneNode::body_part::UPPER_ARM_LEFT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = { 0.0f, 0.0f, -0.025f },
+						.rotation = { 1.0f, 0.025f, 0.0f, 0.0f },
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::LOWER_ARM_LEFT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = {},
+						.rotation = {},
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::UPPER_ARM_RIGHT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = { 0.0f, 0.0f, 0.025f },
+						.rotation = { 1.0f, -0.025f, 0.0f, 0.0f },
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::LOWER_ARM_RIGHT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = {},
+						.rotation = {},
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::UPPER_LEG_LEFT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = { -0.05f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, -0.05f },
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::LOWER_LEG_LEFT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = { 0.05f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, 0.1f },
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::UPPER_LEG_RIGHT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = { -0.05f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, -0.05f },
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::LOWER_LEG_RIGHT,
 					{
-						.pos = { 0.0f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = { 0.05f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, 0.1f },
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				}
@@ -102,16 +187,16 @@ std::list<Animator::KeyFrame>	Animator::_createIdle(void)
 				{
 					BoneNode::body_part::CHEST,
 					{
-						.pos = { 0.0f, -0.1f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.pos = {},
+						.rotation = {},
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::UPPER_ARM_LEFT,
 					{
-						.pos = { 0.0f, 0.0f, -0.05f },
-						.rotation = { 1.0f, 0.05f, 0.0f, 0.0f },
+						.pos = {},
+						.rotation = {},
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
@@ -126,8 +211,8 @@ std::list<Animator::KeyFrame>	Animator::_createIdle(void)
 				{
 					BoneNode::body_part::UPPER_ARM_RIGHT,
 					{
-						.pos = { 0.0f, 0.0f, 0.05f },
-						.rotation = { 1.0f, -0.05f, 0.0f, 0.0f },
+						.pos = {},
+						.rotation = {},
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
@@ -142,36 +227,121 @@ std::list<Animator::KeyFrame>	Animator::_createIdle(void)
 				{
 					BoneNode::body_part::UPPER_LEG_LEFT,
 					{
-						.pos = { -0.2f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, -0.1f },
+						.pos = {},
+						.rotation = {},
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::LOWER_LEG_LEFT,
 					{
-						.pos = { 0.55f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.2f },
+						.pos = {},
+						.rotation = {},
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::UPPER_LEG_RIGHT,
 					{
-						.pos = { -0.2f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, -0.1f },
+						.pos = {},
+						.rotation = {},
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				},
 				{
 					BoneNode::body_part::LOWER_LEG_RIGHT,
 					{
-						.pos = { 0.55f, 0.0f, 0.0f },
-						.rotation = { 1.0f, 0.0f, 0.0f, 0.2f },
+						.pos = {},
+						.rotation = {},
 						.scaling = { 1.0f, 1.0f, 1.0f }
 					}
 				}
 			}
 		},
+		{
+			.duration = 0.5f,
+			.movement = {
+				{
+					BoneNode::body_part::HEAD,
+					{
+						.pos = { 0.0f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.025f, 0.0f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::CHEST,
+					{
+						.pos = { 0.0f, -0.1f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_ARM_LEFT,
+					{
+						.pos = { 0.0f, 0.0f, -0.025f },
+						.rotation = { 1.0f, 0.025f, 0.0f, 0.0f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_ARM_LEFT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_ARM_RIGHT,
+					{
+						.pos = { 0.0f, 0.0f, 0.025f },
+						.rotation = { 1.0f, -0.025f, 0.0f, 0.0f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_ARM_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_LEG_LEFT,
+					{
+						.pos = { -0.05f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, -0.05f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_LEG_LEFT,
+					{
+						.pos = { 0.05f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, 0.1f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_LEG_RIGHT,
+					{
+						.pos = { -0.05f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, -0.05f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_LEG_RIGHT,
+					{
+						.pos = { 0.05f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, 0.1f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				}
+			}
+		}
 	};
 }
