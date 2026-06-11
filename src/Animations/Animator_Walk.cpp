d43e1d9bf2c411b25a666f5bee776d5ffd92bc7e
Victor Groux <victorgroux99@gmail.com>
2026-06-11 11:56:19 +0200

    Walking animation seems pretty OK

--- src/Animations/Animator_Walk.cpp
+++ src/Animations/Animator_Walk.cpp
@@ -3,8 +3,8 @@
 std::list<Animator::KeyFrame>	Animator::_createWalk(void)
 {
 	return {
-		{
-			.duration = 0.5f,
+		{	// Default position
+			.duration = 0.1f,
 			.movement = {
 				{
 					BoneNode::body_part::HEAD,
@@ -88,5 +88,262 @@ std::list<Animator::KeyFrame>	Animator::_createWalk(void)
 				}
 			}
 		},
+		{	// Left arm and right leg going forward
+			// Right arm and left leg going back
+			.duration = 0.3f,
+			.movement = {
+				{
+					BoneNode::body_part::HEAD,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::CHEST,
+					{
+						.pos = { 0.0f, -0.05f, 0.0f },
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_ARM_LEFT,
+					{
+						.pos = {},
+						.rotation = { 1.0f, 0.0f, 0.0f, -0.5f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_ARM_LEFT,
+					{
+						.pos = { -0.1f, 0.05f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.1f, -0.15f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_ARM_RIGHT,
+					{
+						.pos = {},
+						.rotation = { 1.0f, 0.0f, 0.0f, 0.3f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_ARM_RIGHT,
+					{
+						.pos = {},
+						.rotation = { 1.0f , 0.0f, 0.0f, -0.05f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_LEG_LEFT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_LEG_LEFT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_LEG_RIGHT,
+					{
+						.pos = { -0.1f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, -0.3f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_LEG_RIGHT,
+					{
+						.pos = { 0.2f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, 0.3f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				}
+			}
+		},
+		{	// Default position
+			.duration = 0.1f,
+			.movement = {
+				{
+					BoneNode::body_part::HEAD,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::CHEST,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_ARM_LEFT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_ARM_LEFT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_ARM_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_ARM_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_LEG_LEFT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_LEG_LEFT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_LEG_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_LEG_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				}
+			}
+		},
+		{	// Right arm and left leg going forward
+			// Left arm and right leg going back
+			.duration = 0.3f,
+			.movement = {
+				{
+					BoneNode::body_part::HEAD,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::CHEST,
+					{
+						.pos = { 0.0f, -0.05f, 0.0f },
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_ARM_LEFT,
+					{
+						.pos = {},
+						.rotation = { 1.0f, 0.0f, 0.0f, 0.3f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_ARM_LEFT,
+					{
+						.pos = {},
+						.rotation = { 1.0f , 0.0f, 0.0f, -0.05f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_ARM_RIGHT,
+					{
+						.pos = {},
+						.rotation = { 1.0f, 0.0f, 0.0f, -0.5f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_ARM_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_LEG_LEFT,
+					{
+						.pos = { -0.1f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, -0.3f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_LEG_LEFT,
+					{
+						.pos = { 0.2f, 0.0f, 0.0f },
+						.rotation = { 1.0f, 0.0f, 0.0f, 0.3f },
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::UPPER_LEG_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				},
+				{
+					BoneNode::body_part::LOWER_LEG_RIGHT,
+					{
+						.pos = {},
+						.rotation = {},
+						.scaling = { 1.0f, 1.0f, 1.0f }
+					}
+				}
+			}
+		}
 	};
 }
